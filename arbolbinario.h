009fecb2ba2340ebe6098f510450039cc2953dda
Cristian Infante <71674005+Cristian-Infante@users.noreply.github.com>
2022-03-06 17:08:21 -0500

    Update arbolbinario.h

--- arbolbinario.h
+++ arbolbinario.h
@@ -4,30 +4,38 @@
 
 template <class T>
 class ArbolBinario{
-
     private:
         Nodo<T> *raiz;
         Nodo<T> *padre;
-
     public:
         ArbolBinario();
        ~ArbolBinario();
        void DestruirArbol(Nodo<T> *);
        void setRaiz(Nodo<T> *);
        Nodo<T> *getRaiz();
+       T Padre(T);
        void setPadre(Nodo<T> *);
        Nodo<T> *getPadre();
        bool estaVacio();
        void Insertar(T);
-       void Preorden(Nodo<T> *); // RECORRER  EL
-       void Inorden(Nodo<T> *); //  ARBOL
-       void Posorden(Nodo<T> *); //  INCLUYE MOSTRAR
-       //CONTAR EL NIVEL Y LA ALTURA DEL ARBOL
+       void Preorden(Nodo<T> *);
+       void Inorden(Nodo<T> *);
+       void Posorden(Nodo<T> *);
        Nodo<T> *Buscar(T);
-       bool BuscarB(T);
-       void Eliminar(T);
-       int Nivel(T, int);
-       bool Camino(T, T);
+       Nodo<T> *Buscar2(Nodo<T> *, T);
+       Nodo<T> *BuscarReemp(Nodo<T> *);
+       Nodo<T> *Eliminar(T);
+       int Grado(T);
+       int Nivel(T);
+       int Camino(T, T); //bool
+       int Altura(Nodo<T> *);
+       void MostrarxNiveles();
+       void MostrarNivel(Nodo<T> *, int);
+       int Isomorfos(Nodo<T> *, Nodo<T> *); //bool
+       Nodo<T> *RD(Nodo<T> *);
+       Nodo<T> *RI(Nodo<T> *);
+       Nodo<T> *DRD(Nodo<T> *);
+       Nodo<T> *DRI(Nodo<T> *);
+       Nodo<T> *Balanceo(Nodo<T> *);
 };
-
 #endif // ARBOLBINARIO_H
