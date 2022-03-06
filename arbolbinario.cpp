a677d476922c010146161a78afb94e990ddb023d
Cristian Infante <71674005+Cristian-Infante@users.noreply.github.com>
2022-03-06 17:07:49 -0500

    Update arbolbinario.cpp

--- arbolbinario.cpp
+++ arbolbinario.cpp
@@ -12,7 +12,6 @@ ArbolBinario<T>::~ArbolBinario(){
 
 template <typename T>
 void ArbolBinario<T>::DestruirArbol(Nodo<T> *aux){
-    //CASO BASE
     if(aux!=nullptr){
         DestruirArbol(aux->getIzq());
         DestruirArbol(aux->getDer());
@@ -37,6 +36,25 @@ template <typename T>
 bool ArbolBinario<T>::estaVacio(){
     return(raiz==nullptr);
 }
+
+template <typename T>
+T ArbolBinario<T>::Padre(T dato){
+    Nodo<T> *ant = nullptr;
+    Nodo<T> *act = raiz;
+    while((act != nullptr)&&(act->getInfo() != dato)){
+        ant = act;
+        if(act->getInfo() > dato){
+            act = act->getIzq();
+        }
+        else{
+            act = act->getDer();
+        }
+    }
+    if(act != nullptr){
+        return ant->getInfo();
+    }
+}
+
 template <typename T>
 void ArbolBinario<T>::setPadre(Nodo<T> *nuevo){
     padre = nuevo;
@@ -125,58 +143,254 @@ void ArbolBinario<T>::Posorden(Nodo<T> *aux){
 
 template <typename T>
 Nodo<T> *ArbolBinario<T>::Buscar(T dato){
-    Nodo<T> *padreT = getPadre();
-    if (estaVacio()){
+    Nodo<T> *aux = raiz;
+    while((aux != nullptr)&&(aux->getInfo() != dato)){
+        if(aux->getInfo() > dato){
+            aux = aux->getIzq();
+        }
+        else{
+            aux = aux->getDer();
+        }
+    }
+    return aux;
+}
+
+template <typename T>
+Nodo<T> *ArbolBinario<T>::Buscar2(Nodo<T> *aux, T dato){
+    if((aux != nullptr)&&(aux->getInfo() != dato)){
+        if(aux->getInfo() > dato){
+            return Buscar2(aux->getIzq(),dato);
+        }
+        if(aux->getInfo() < dato){
+            return Buscar2(aux->getDer(),dato);
+        }
+    }
+}
+
+template <typename T>
+Nodo<T>* ArbolBinario<T>::BuscarReemp(Nodo<T> *aux){
+    switch(Grado(aux->getInfo())){
+        case 0:
+            return nullptr;
+            break;
+        case 1:
+            if(aux->getIzq() != nullptr){
+                return aux->getIzq();
+            }
+            else{
+                return aux->getDer();
+            }
+            break;
+        case 2:
+            Nodo<T> *ant = nullptr;
+            Nodo<T> *act = aux->getIzq();
+            while(act->getDer() != nullptr){
+                ant = act;
+                act = act->getDer();
+            }
+            if(ant != nullptr){
+                ant->setDer(act->getIzq());
+                act->setIzq(aux->getIzq());
+            }
+            act->setDer(aux->getDer());
+            return act;
+    }
+}
+
+template <typename T>
+Nodo<T>* ArbolBinario<T>::Eliminar(T dato){
+    Nodo<T> *padre = nullptr;
+    Nodo<T> *salir = raiz;
+    while((salir != nullptr)&&(salir->getInfo() != dato)){
+        padre = salir;
+        if(salir->getInfo() > dato){
+            salir = salir->getIzq();
+        }
+        else{
+            salir = salir->getDer();
+        }
+    }
+    if(salir != nullptr){
+        Nodo<T> *reemp = BuscarReemp(salir);
+        if(padre != nullptr){
+            if(padre->getInfo() > dato){
+                padre->setIzq(reemp);
+            }
+            else{
+                padre->setDer(reemp);
+            }
+        }
+        else{
+            raiz = reemp;
+        }
+    }
+}
+
+template <typename T>
+int ArbolBinario<T>::Grado(T dato){
+    Nodo<T> *aux = Buscar(dato);
+    if(aux != nullptr){
+        int cont = 0;
+        if(aux->getIzq() != nullptr){
+            cont++;
+        }
+        if(aux->getDer() != nullptr){
+            cont++;
+        }
+        return cont;
+    }
+    return -1;
+}
+
+template <typename T>
+int ArbolBinario<T>::Nivel(T dato){
+    Nodo<T> *aux = raiz;
+    int cont = 0;
+    while((aux != nullptr)&&(aux->getInfo() != dato)){
+        cont++;
+        if(aux->getInfo() > dato){
+            aux = aux->getIzq();
+        }
+        else{
+            aux = aux->getDer();
+        }
+    }
+    if(aux != nullptr){
+        return cont;
+    }
+    else {
+        return  -1;
+    }
+}
+
+template <typename T>
+int ArbolBinario<T>::Camino(T dato1, T dato2){
+    Nodo<T> *valor1 = Buscar(dato1);
+    if(valor1 != nullptr){
+        Nodo<T> *valor2 = Buscar2(valor1, dato2);
+        if(valor2 != nullptr){
+            return 1;
+        }
+        else{
+            return 0;
+        }
+    }
+}
+
+template <typename T>
+int ArbolBinario<T>::Altura(Nodo<T> *aux){
+    int altIZ, altDE;
+    if(aux != nullptr){
+        altIZ = Altura(aux->getIzq());
+        altDE = Altura(aux->getDer());
+        if(altIZ > altDE){
+            return altIZ+1;
+        }
+        else{
+            return altDE+1;
+        }
+    }
+    else{
         return 0;
     }
-    if(padreT->getInfo()<dato){
-        setPadre(padreT->getDer());
-        Buscar(dato);
-    }
-    else if(padreT->getInfo()>dato){
-        setPadre(padreT->getDer());
-        Buscar(dato);
-    }
-    else {
-        return  padreT;
+}
+
+template <typename T>
+void ArbolBinario<T>::MostrarxNiveles(){
+    Nodo<T> *aux = raiz;
+    int alt = Altura(aux);
+    for(int i=0; i<alt; i++){
+        cout<<endl<<"\tNivel "<<i<<":\t";
+        MostrarNivel(aux, i);
     }
 }
 
 template <typename T>
-bool ArbolBinario<T>::BuscarB(T dato){
-    Nodo<T> *padreT = getPadre();
-    if (estaVacio()){
-        return false;
+void ArbolBinario<T>::MostrarNivel(Nodo<T> * aux, int nivel){
+    if((aux != nullptr)&&(nivel !=0)){
+        MostrarNivel(aux->getIzq(), nivel-1);
+        MostrarNivel(aux->getDer(), nivel-1);
     }
-    if((padre->getInfo()!=NULL)&&(padre->getInfo()!=dato)){
-        if(padreT->getInfo()<dato){
-            setPadre(padreT->getDer());
-            Buscar(dato);
+    else{
+        if(aux != nullptr){
+            cout<<aux->getInfo()<<"\t";
         }
-        else if(padreT->getInfo()>dato){
-            setPadre(padreT->getDer());
-            Buscar(dato);
-        }
-    }
-    if(padre->getInfo()==dato){
-        return true;
     }
 }
 
 template <typename T>
-int ArbolBinario<T>::Nivel(T dato, int nivel){
-    Nodo<T> *padreT = getPadre();
-    if(padreT->getInfo()<dato){
-        setPadre(padreT->getDer());
-        nivel++;
-        Nivel(dato, nivel);
+int ArbolBinario<T>::Isomorfos(Nodo<T> *arb1, Nodo<T> *arb2){
+    if((arb1 == nullptr)&&(arb2 == nullptr)){
+        return 1;
     }
-    else if(padreT->getInfo()>dato){
-        setPadre(padreT->getDer());
-        nivel++;
-        Nivel(dato, nivel);
+    if(((arb1->getIzq() != nullptr)&&(arb2->getIzq() == nullptr)) || ((arb1->getIzq() == nullptr)&&(arb2->getIzq() != nullptr)) || ((arb1->getDer() != nullptr)&&(arb2->getDer() == nullptr)) || ((arb1->getDer() == nullptr)&&(arb2->getDer() != nullptr))){
+        return 0;
     }
-    else {
-        return  nivel;
+    else{
+        return 1;
+    }
+}
+
+template <typename T>
+Nodo<T>* ArbolBinario<T>::RD(Nodo<T> *p){
+    Nodo<T>* q = p->getIzq();
+    p->setIzq(q->getDer());
+    q->setDer(p);
+    return q;
+}
+
+template <typename T>
+Nodo<T>* ArbolBinario<T>::RI(Nodo<T>* p) {
+    Nodo<T>* q = p->getDer();
+    p->setDer(q->getIzq());
+    q->setIzq(p);
+    return q;
+}
+
+template <typename T>
+Nodo<T>* ArbolBinario<T>::DRD(Nodo<T>* p) {
+    Nodo<T>* q = p->getIzq();
+    p->setIzq(RI(q));
+    return RD(p);
+}
+
+template <typename T>
+Nodo<T>* ArbolBinario<T>::DRI(Nodo<T>* p) {
+    Nodo<T>* q = p->getDer();
+    p->setDer(RD(q));
+    return RI(p);
+}
+
+template <typename T>
+Nodo<T>* ArbolBinario<T>::Balanceo(Nodo<T> *p){
+    if(p != nullptr){
+        /**/Preorden(p); cout<<endl;
+        p->setIzq(Balanceo(p->getIzq()));
+        p->setDer(Balanceo(p->getDer()));
+        int dif = Altura(p->getIzq()) - Altura(p->getDer());
+        //cout<<"\tDiferencia\t"<<Altura(p->getIzq()) - Altura(p->getDer())<<endl<<endl;
+        if(abs(dif) > 1){
+            /**/cout<<"entro";
+            if(dif > 0){
+                Nodo<T>* q = p->getIzq();
+                int difq = Altura(q->getIzq()) - Altura(q->getIzq());
+                if(difq >= 0){
+                    return RD(p);
+                }
+                else{
+                    return DRD(p);
+                }
+            }
+            else{
+                Nodo<T>* q = p->getDer();
+                int difq = Altura(q->getIzq()) - Altura(q->getDer());
+                if (difq < 0) {
+                    return RI(p);
+                }
+                else {
+                    return DRI(p);
+                }
+            }
+        }
     }
 }
